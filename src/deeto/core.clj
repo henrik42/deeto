12315f5da5f866d926873f1caa0d32be3cb07534
henrik42 <nomail>
2019-05-27 22:19:04 +0200

    core

--- src/deeto/core.clj
+++ src/deeto/core.clj
@@ -1,57 +1,78 @@
 (ns deeto.core)
 
-(defn ser-de-ser [x]
+(defn ser-de-ser
+  "Returns a deep clone. The clone is created by serializing and
+   deserializing any non-nil argument. Checks that the argument and
+   the clone are `Arrays/deepEquals`. Returns `nil` for `nil`."
+
+  [x]
   {:post [(java.util.Arrays/deepEquals (into-array Object [x]) (into-array Object [%]))]}
   (if (nil? x) nil
-      (let [baos (java.io.ByteArrayOutputStream.)
-            oos (java.io.ObjectOutputStream. baos)]
+      (with-open [baos (java.io.ByteArrayOutputStream.)
+                  oos (java.io.ObjectOutputStream. baos)]
         (.writeObject oos x)
-        (.readObject (java.io.ObjectInputStream. (java.io.ByteArrayInputStream. (.toByteArray baos)))))))
+        (with-open [bais (java.io.ByteArrayInputStream. (.toByteArray baos))
+                    ois (java.io.ObjectInputStream. bais)]
+          (.readObject ois)))))
 
 (defn reflect-on
   "Inspects via reflection all methods of the DTO `clazz` and
-  determines the _properties_ by looking at getter and setter methods.
+   determines the __properties__ by looking at __getter__ and
+   __setter__ methods.
 
-  Throws if any of the properties are _inconsistent_ (e.g. type
-  mismatch between getter and setter)."
+   Throws if any of the properties are __inconsistent__ (e.g. type
+   mismatch between getter and setter, wrong numer ob arguments, wrong
+   return type)."
 
   [clazz]
   (reduce
    (fn [res {:keys [method-name get-property set-property return-type parameter-types] :as m}]
-     ;; Object.getClass() is not a DTO property! TBD: do we need this?
+     ;; Object.getClass() is not a DTO property-getter! TBD: do we
+     ;; need this?
      (if-let [property-name (let [x (or get-property set-property)]
                               (when-not (= "Class" x) x))]
        ;; method is a getter or setter
        (assoc res property-name 
               (if-let [{:keys [property-type property-getter property-setter] :as p} (res property-name)]
+                ;; we've seen the property before
                 (assoc p
                   :property-getter
                   (if-not get-property property-getter
                           (cond
                             property-getter
                             (throw (ex-info "Duplicate getter" {:res res :m m}))
+                            
                             (not= property-type return-type)
                             (throw (ex-info "Getter's type mismatch" {:res res :m m}))
+                            
                             (or 
                              (not (empty? parameter-types))
                              (= java.lang.Void/TYPE return-type))
                             (throw (ex-info "Not a valid getter" {:res res :m m}))
+                            
                             :else method-name))
+                  
                   :property-setter
                   (if-not set-property property-setter
                           (cond
                             property-setter
                             (throw (ex-info "Duplicate setter" {:res res :m m}))
+                            
                             (not= property-type (first parameter-types))
                             (throw (ex-info "Setter's type mismatch" {:res res :m m}))
+                            
                             (or 
                              (not= 1 (count parameter-types))
                              (not= java.lang.Void/TYPE return-type))
                             (throw (ex-info "Not a valid setter" {:res res :m m}))
+                            
                             :else method-name)))
+
+                ;; seeing the property the first time
                 {:property-name property-name
                  :property-type (if set-property (first parameter-types) return-type)
                  :property-getter (when get-property
+                                    ;; DRY! This repeats the stuff from aboe
                                     (cond
                                       (or 
                                        (not (empty? parameter-types))
@@ -60,16 +81,19 @@
                                       :else method-name))
                  :property-setter (when set-property
                                     (cond
+                                      ;; DRY! This repeats the stuff from aboe
                                       (or 
                                        (not= 1 (count parameter-types))
                                        (not= java.lang.Void/TYPE return-type))
                                       (throw (ex-info "Not a valid setter" {:res res :m m}))
                                       :else method-name))}))
-       ;; Method is non-getter/setter (like equals, hashCode, clone, readResolve,...)
-       (do
-         (println "no getter/setter" m)
+       ;; Method is non-getter/setter (like equals, hashCode, clone,
+       ;; readResolve,...) So we ignore it here! We could check that
+       ;; it is one of the known other methods that we support.
+       (do 
+         ;; (println "no getter/setter" m)
          res)))
-   {} ;; Start value is an empty map
+   (sorted-map) ;; Start value is an empty map
    (map (fn [m] ;; seq of method-map containing preprocessed things about each method
           (let [n (.getName m)]
             {:method-name n
@@ -82,12 +106,22 @@
 (declare handler)
 
 (defn make-proxy
+  "Returns a Java dynamic proxy (_Deeto proxy_) for the given
+   interface(s). Any invocation on the proxy will be delegated to the
+   `handler-fn` which should be a function of `the-method` and
+   `the-args` of `java.lang.reflect.InvocationHandler/invoke`.
+
+   Calling the overloaded 1-arity variant will create a `handler-fn`
+   which keeps an internal state `atom` and delegates calls and the
+   state to `handler` (see below)."
+  
   ([proxy-type]
      (let [properties (reflect-on proxy-type)
            state (atom (into (sorted-map)
                              (map #(-> [% nil]) (keys properties))))]
        (make-proxy proxy-type
                    (partial #'handler proxy-type properties state))))
+  
   ([proxy-type handler-fn]
      (let [pt (if (vector? proxy-type)
                 (into-array proxy-type)
@@ -106,20 +140,29 @@
   (.invoke (java.lang.reflect.Proxy/getInvocationHandler p)
            nil nil nil))
 
-(defn handle-equals [state the-method the-args]
+(defn handle-equals
+  "Returns `true` if `@state` and the first value of `the-args` are
+   equal in terms of `java.util.Arrays/deepEquals`."
+
+  [state the-method the-args]
   (let [the-args (into [] the-args)
         other (first the-args)]
     (cond 
-      (not= 1 (count the-args)) (throw (ex-info "Not a valid equals method" {:the-method the-method :the-args the-args}))
-      (nil? other) false
+      (not= 1 (count the-args))
+      (throw (ex-info "Not a valid equals method" {:the-method the-method :the-args the-args}))
+      
+      (nil? other)
+      false
+      
       :else 
-      #_
-      (let [s1 (into-array Object (seq @state))
-            s2 (into-array Object (seq (get-state other)))]
-        (println "states=" (into [] s1) (into [] s2))
-        (java.util.Arrays/deepEquals s1 s2))
       ;; #_
+      (let [s1 (into-array Object (vals @state))
+            s2 (into-array Object (vals (get-state other)))]
+        ;; (println "states=" (into [] s1) (into [] s2))
+        (java.util.Arrays/deepEquals s1 s2))
+      #_ ;; altenative -- should be removed. 
       (reduce (fn [v [p v1 v2]]
+                #_
                 {:pre [(or (println "compare" [p v1 v2]) true)]
                  :post [(or (println "compare" [p v1 v2] " -> " %) true)]}
                 (if-not 
@@ -132,7 +175,6 @@
                    @state (get-state other))))))
 
 (defn handler [clazz properties state the-method the-args]
-  ;; (println "call" the-method the-args)
   ;; Special access to @state via null method (see get-state)
   (if-not the-method @state
           (let [method-name (.getName the-method)
@@ -142,25 +184,29 @@
                 parameter-types (into [] (.getParameterTypes the-method))]
             (cond
               
-              ;; TBD: return clone!
               get-property (ser-de-ser (@state get-property))
-              
               set-property (swap! state assoc set-property (ser-de-ser (first the-args)))
               
               (= "toString" method-name) (str @state)
-              (= "equals" method-name) (handle-equals state the-method the-args)
+              (= "equals" method-name) (and
+                                        ;; classes match?
+                                        (.equals clazz nil)
+                                        (handle-equals state the-method the-args))
               (= "hashCode" method-name) (java.util.Arrays/deepHashCode (into-array Object (vals @state)))
 
-              ;; TBD: create clone of vals
+              ;; Note: for clone we do not need to create a deep copy!
+              ;; Since we'll never mutate internal values and we never
+              ;; leak references to the outside there is no danger
+              ;; that anyone ever changes state. So effectivly we hava
+              ;; constant values (which you need not clone).
               (= "clone" method-name)
               (make-proxy clazz (partial #'handler clazz properties (atom @state)))
               
-              :else (throw (ex-info "oops" {:properties properties
-                                            :state state
-                                            :the-method the-method
-                                            :the-args (into [] the-args)}))))))
-
-
+              :else (throw (ex-info "Unknown invocation"
+                                    {:properties properties
+                                     :state state
+                                     :the-method the-method
+                                     :the-args (into [] the-args)}))))))
 #_
 (defprotocol Factory
   (newInstance [this]))
