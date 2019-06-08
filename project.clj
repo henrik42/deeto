54ba19335c993de9058c92ae3d56f8b8f64ab7b8
henrik42 <nomail>
2019-06-08 10:10:48 +0200

    deploy

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
+;; re-enables http repository support in Leiningen 2.8
+(require 'cemerick.pomegranate.aether)
+(cemerick.pomegranate.aether/register-wagon-factory!
+ "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
+
 (defproject deeto "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
@@ -5,6 +10,22 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :java-source-paths ["java/test" "java/src"]
-  :aot [deeto.java-api]
+  :target-path "target/%s"
+  :aot [deeto.java-api
+        deeto.core]
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-swank "1.4.5"]])
+  :plugins [[lein-swank "1.4.5"]]
+    
+  :aliases {"deploy"  ["do" "clean," "deploy"]}
+  :profiles {;; run a local Nexus in a docker container with:
+             ;; docker run -d -p 8081:8081 --name nexus sonatype/nexus:oss
+             ;;
+             ;; Then you can deploy a SNAPSHOT or release via
+             ;; lein with-profile +local deploy
+             :local {:repositories [["snapshots" {:url "http://localhost:8081/nexus/content/repositories/snapshots/"
+                                                  :sign-releases false :username "admin" :password "admin123"}]
+                                    ["releases" {:url "http://localhost:8081/nexus/content/repositories/releases/"
+                                                 :sign-releases false :username "admin" :password "admin123"}]]}})
+
+            
+
